94526e12708eb59c5381ea0a7c6c563b8fc1e27d
Bonno9893 <filippo.bonafe98@gmail.com>
2024-05-08 16:07:08 +0200

    Sesto Commit

--- main.py
+++ main.py
@@ -12,71 +12,105 @@ logger = logging.getLogger(__name__)
 vision_client = vision.ImageAnnotatorClient()
 storage_client = storage.Client()
 bucket = storage_client.get_bucket('photo_chatbot')
+bucket_name = 'photo_chatbot'
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Ciao, inviami un\'immagine o chiedimi di cercare immagini basate su descrizioni!')
+    update.message.reply_text('Ciao, inviami un\'immagine!')
 
 def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
         'Ciao, mi presento, sono Photo Chatbot! Il mio compito è di memorizzare le immagini inviate dagli utenti e di recuperarle tramite la loro descrizione. Inviami un\'immagine per iniziare.')
 
 def handle_photo(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
-    photo_file = update.message.photo[-1].get_file()
-    photo_bytes = photo_file.download_as_bytearray()
-    photo_bytes = bytes(photo_bytes)  # Conversione a bytes
+    try:
+        user_id = update.message.from_user.id
+        photo_file = update.message.photo[-1].get_file()
+        photo_bytes = photo_file.download_as_bytearray()
+        photo_bytes = bytes(photo_bytes)  # Conversione a bytes
 
-    # Analizza l'immagine per ottenere le etichette
-    image = vision.Image(content=photo_bytes)
-    response = vision_client.label_detection(image=image)
-    labels = [label.description for label in response.label_annotations]
+        # Crea il client per Google Cloud Storage utilizzando le credenziali dal file JSON
+        client = storage.Client.from_service_account_json('photo-chatbot-1-16d5d9d04aaa.json')
 
-    # Genera un nome univoco per il file basato su user_id e photo_file_id
-    photo_file_id = photo_file.file_id
-    file_name = f'{user_id}/{photo_file_id}.jpg'
+        # Analizza l'immagine per ottenere le etichette
+        image = vision.Image(content=photo_bytes)
+        response = vision_client.label_detection(image=image)
+        labels = [label.description.lower() for label in response.label_annotations]  # Converti le etichette in minuscolo
 
-    # Carica l'immagine su Google Cloud Storage
-    blob = bucket.blob(file_name)
-    blob.metadata = {'labels': ','.join(labels)}  # Salva le etichette come metadati
-    blob.upload_from_string(photo_bytes, content_type='image/jpeg')
+        # Genera un nome univoco per il file basato su user_id e photo_file_id
+        file_name = f'{user_id}/{photo_file.file_id}.jpg'
 
-    # Conferma all'utente che l'immagine è stata salvata
-    update.message.reply_text('Immagine salvata con successo e etichettata!')
+        # Carica l'immagine su Google Cloud Storage
+        bucket = client.bucket('photo_chatbot')  # Seleziona il bucket corretto
+        blob = bucket.blob(file_name)
+        blob.metadata = {'labels': ','.join(labels)}  # Salva le etichette come metadati
+        blob.upload_from_string(photo_bytes, content_type='image/jpeg')
 
-def search_images(update: Update, context: CallbackContext) -> None:
-    query = update.message.text.lower().replace('cerca immagine', '').strip()
-    user_id = update.message.from_user.id
-    found_images = []
+        # Conferma all'utente che l'immagine è stata salvata
+        update.message.reply_text('Immagine salvata con successo e etichettata!')
+    except Exception as e:
+        logger.error(f"Errore durante il caricamento dell'immagine: {str(e)}")
+        update.message.reply_text('Si è verificato un errore durante il caricamento dell\'immagine.')
 
-    # Scansiona tutte le immagini salvate per questo utente
-    blobs = list(bucket.list_blobs(prefix=f'{user_id}/'))
-    for blob in blobs:
-        blob.reload()  # Ricarica i metadati del blob
-        labels = blob.metadata['labels'] if blob.metadata and 'labels' in blob.metadata else ''
-        if query in labels.split(','):
-            found_images.append(blob.public_url)
 
-    if found_images:
-        for img_url in found_images:
-            update.message.reply_photo(photo=img_url)
-    else:
-        update.message.reply_text('Nessuna immagine trovata per la tua ricerca.')
+def search_single_image(update: Update, context: CallbackContext) -> None:
+    search_images(update, context, single=True)
+
+def search_all_images(update: Update, context: CallbackContext) -> None:
+    search_images(update, context, single=False)
+
+def search_images(update: Update, context: CallbackContext, single=True):
+    try:
+        user_id = update.message.from_user.id
+        command_text = update.message.text.lower()
+        if "cerca tutte le immagini" in command_text:
+            query = command_text.replace('cerca tutte le immagini', '').strip()
+            single = False
+        else:
+            query = command_text.replace('cerca immagine', '').strip()
+            single = True
+
+        # Scansiona tutte le immagini salvate per questo utente
+        blobs = list(bucket.list_blobs(prefix=f'{user_id}/'))
+        found_any = False  # Flag per verificare se abbiamo trovato almeno una immagine
+        for blob in blobs:
+            blob.reload()  # Ricarica i metadati del blob
+            labels = blob.metadata.get('labels', '').lower()  # Converti le etichette in minuscolo
+            if query in labels:
+                found_any = True
+                image_bytes = blob.download_as_bytes()
+                context.bot.send_photo(chat_id=update.message.chat_id, photo=image_bytes)
+                if single:
+                    return  # Se è richiesta solo una foto, interrompe dopo il primo match
+
+        if not found_any:
+            update.message.reply_text('Nessuna immagine trovata per la tua ricerca.')
+    except Exception as e:
+        logger.error(f"Errore durante la ricerca delle immagini: {str(e)}")
+        update.message.reply_text('Si è verificato un errore durante la ricerca delle immagini.')
+
+
+
 
 def error(update, context):
     """Logga gli errori causati dagli aggiornamenti."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def main():
-    """Start the bot."""
+def main() -> None:
+    print('Il Bot è partito...')
     updater = Updater(bot_token, use_context=True)
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("cerca_immagine", search_single_image))
+    dp.add_handler(CommandHandler("cerca_tutte_le_immagini", search_all_images))
+    dp.add_handler(MessageHandler(Filters.text & Filters.regex(r'(?i)^cerca immagine.*'), search_single_image))
+    dp.add_handler(MessageHandler(Filters.text & Filters.regex(r'(?i)^cerca tutte le immagini.*'), search_all_images))
     dp.add_handler(MessageHandler(Filters.photo, handle_photo))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, search_images))
 
-    # Avvia il bot
+    # Registrazione del gestore di errori
+    dp.add_error_handler(error)
+
     updater.start_polling()
     updater.idle()
 
